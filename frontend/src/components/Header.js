5af36c769a1831b9c1ee7d21ed63e56de65ef4fd
Johnny Squardov <johnny@johnmichael.dev>
2021-04-08 09:27:09 -0400

    Setting up backend Django

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { Navbar, Nav, Container } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";	
 
 const Header = () => {
 	return (
